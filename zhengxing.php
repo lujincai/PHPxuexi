9c3496afcfffd2bfbca7235dc5f1db559614b67e
lujincai <49512266+lujincai@users.noreply.github.com>
2019-04-28 20:41:41 +0800

    Create zhengxing.php

---
+++ zhengxing.php
@@ -0,0 +1,19 @@
+<!DOCTYOPE html>
+<html> 
+<body>
+ <?php
+ $x=5985;
+ var_dump($x);
+ echo"<br>";
+ $x=-345;//负数
+ var-dump($x);
+ echo"<br>";
+ $x=0x8c;//十六进制数
+ var-dump($x);
+ echo"<br>";
+ $x=047;//八进制数
+ var-dump($x);
+ ?>
+ </body>
+ </html>
+ 
