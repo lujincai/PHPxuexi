e53529f8abb97b73a1c1c16c8148b22d90031599
lujincai <49512266+lujincai@users.noreply.github.com>
2019-05-05 09:33:50 +0800

    Add files via upload

---
+++ fudianxing.php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+<body>
+<?php
+$x=10.365;
+val-dump($x);
+echo"<br>";
+$x=2.4e3;
+val_dump($x);
+echo"<br>";
+$x=8E-5;
+val_dump($x);
+?>
+</body>
+</html>
